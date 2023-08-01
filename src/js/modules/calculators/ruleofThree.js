35fcc9d14003e51cb3e9f01cddd0cdd677e4131d
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-08-01 08:18:16 -0300

    feat: add copy to clipboard for rule of 3

--- src/js/modules/calculators/ruleofThree.js
+++ src/js/modules/calculators/ruleofThree.js
@@ -5,6 +5,11 @@ class RuleOfThree {
     this.elementC = document.querySelector('#ruleofthree-c');
     this.elementX = document.querySelector('#ruleofthree-x');
 
+    this.buttonCopy = document.querySelector('#copy');
+
+    this.copyPassword = this.copyPassword.bind(this);
+    this.updateResult = this.updateResult.bind(this);
+
     this.elementError = document.querySelector('.ruleofthree__error');
 
     this.addEventListeners();
@@ -20,10 +25,17 @@ class RuleOfThree {
     return result;
   }
 
+  async copyPassword(e) {
+    e.preventDefault();
+    if (navigator.clipboard)
+      await navigator.clipboard.writeText(this.elementX.value);
+  }
+
   addEventListeners() {
-    this.elementA.addEventListener('input', () => this.updateResult());
-    this.elementB.addEventListener('input', () => this.updateResult());
-    this.elementC.addEventListener('input', () => this.updateResult());
+    this.elementA.addEventListener('input', this.updateResult);
+    this.elementB.addEventListener('input', this.updateResult);
+    this.elementC.addEventListener('input', this.updateResult);
+    this.buttonCopy.addEventListener('click', this.copyPassword);
   }
 
   updateResult() {
@@ -35,20 +47,23 @@ class RuleOfThree {
       this.elementX.value = '0';
       this.elementError.style.display = 'block';
       this.elementError.textContent = 'The values cannot be zero.';
+      this.buttonCopy.disabled = true;
     } else {
       this.elementError.style.display = 'none';
 
       if (this.elementA.value && this.elementB.value && this.elementC.value) {
         const result = this.calculate();
         this.elementX.value = parseFloat(result).toFixed(3);
+        this.buttonCopy.disabled = false;
       }
     }
   }
 
   removeEventListeners() {
-    this.elementA.removeEventListener('input', () => this.updateResult());
-    this.elementB.removeEventListener('input', () => this.updateResult());
-    this.elementC.removeEventListener('input', () => this.updateResult());
+    this.elementA.removeEventListener('input', this.updateResult);
+    this.elementB.removeEventListener('input', this.updateResult);
+    this.elementC.removeEventListener('input', this.updateResult);
+    this.buttonCopy.removeEventListener('click', this.copyPassword);
   }
 }
 
