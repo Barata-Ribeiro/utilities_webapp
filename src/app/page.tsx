181dbd2d89b79b30e469e5a8a72e0258c158c90b
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-09-01 12:45:34 -0300

    build(v2): First Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+    return (
+        <main>
+            <article>
+                <h1>Hello world!</h1>
+                <p className="max-w-2xl">
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+                    labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+                    et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                </p>
+            </article>
+        </main>
+    )
+}
