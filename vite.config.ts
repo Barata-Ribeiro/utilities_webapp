f2e7987f4a3c399d4a86ed663caebb3c768a023f
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-06-20 15:04:12 -0300

    style: refactor code formatting and improve consistency across files

--- vite.config.ts
+++ vite.config.ts
@@ -1,39 +1,27 @@
-import { reactRouter } from "@react-router/dev/vite"
-import tailwindcss from "@tailwindcss/vite"
-import { defineConfig } from "vite"
-import babel from "@rolldown/plugin-babel"
-import react, { reactCompilerPreset } from "@vitejs/plugin-react"
+import { reactRouter } from '@react-router/dev/vite';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
+import babel from '@rolldown/plugin-babel';
+import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 
 export default defineConfig({
-  resolve: { tsconfigPaths: true },
-  plugins: [
-    tailwindcss(),
-    react(),
-    reactRouter(),
-    babel({ presets: [reactCompilerPreset()] }),
-  ],
-  build: {
-    target: "esnext",
-    minify: "oxc",
-    chunkSizeWarningLimit: 1000,
-    rolldownOptions: {
-      output: {
-        entryFileNames: "[hash].js",
-        chunkFileNames: `[hash].js`,
-        assetFileNames: `[hash].[ext]`,
-        minify: true,
-      },
+    resolve: { tsconfigPaths: true },
+    plugins: [tailwindcss(), react(), reactRouter(), babel({ presets: [reactCompilerPreset()] })],
+    build: {
+        target: 'esnext',
+        minify: 'oxc',
+        chunkSizeWarningLimit: 1000,
+        rolldownOptions: {
+            output: {
+                entryFileNames: '[hash].js',
+                chunkFileNames: `[hash].js`,
+                assetFileNames: `[hash].[ext]`,
+                minify: true,
+            },
+        },
+        cssCodeSplit: true,
+        sourcemap: false,
+        assetsInlineLimit: 4096,
     },
-    cssCodeSplit: true,
-    sourcemap: false,
-    assetsInlineLimit: 4096,
-  },
-  assetsInclude: [
-    "**/*.svg",
-    "**/*.png",
-    "**/*.jpg",
-    "**/*.jpeg",
-    "**/*.gif",
-    "**/*.webp",
-  ],
-})
+    assetsInclude: ['**/*.svg', '**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.webp'],
+});
