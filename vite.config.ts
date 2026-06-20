b235000f0b09e254246e413386cf8b4533494a38
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-06-20 15:00:40 -0300

    feat(config): enhance Vite configuration with Babel and React plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,39 @@
 import { reactRouter } from "@react-router/dev/vite"
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "vite"
+import babel from "@rolldown/plugin-babel"
+import react, { reactCompilerPreset } from "@vitejs/plugin-react"
 
 export default defineConfig({
   resolve: { tsconfigPaths: true },
-  plugins: [tailwindcss(), reactRouter()],
+  plugins: [
+    tailwindcss(),
+    react(),
+    reactRouter(),
+    babel({ presets: [reactCompilerPreset()] }),
+  ],
+  build: {
+    target: "esnext",
+    minify: "oxc",
+    chunkSizeWarningLimit: 1000,
+    rolldownOptions: {
+      output: {
+        entryFileNames: "[hash].js",
+        chunkFileNames: `[hash].js`,
+        assetFileNames: `[hash].[ext]`,
+        minify: true,
+      },
+    },
+    cssCodeSplit: true,
+    sourcemap: false,
+    assetsInlineLimit: 4096,
+  },
+  assetsInclude: [
+    "**/*.svg",
+    "**/*.png",
+    "**/*.jpg",
+    "**/*.jpeg",
+    "**/*.gif",
+    "**/*.webp",
+  ],
 })
