efb7c17c5812ed8f75c2270414e39498f689f883
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-06-21 07:43:08 -0300

    feat(pwa): integrate VitePWA plugin and configure PWA settings

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,53 @@ import { defineConfig } from 'vitest/config';
 import babel from '@rolldown/plugin-babel';
 import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import { playwright } from '@vitest/browser-playwright';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig(({ mode }) => {
     const isTest = mode === 'test';
 
     return {
         resolve: { tsconfigPaths: true },
-        plugins: [tailwindcss(), react(), !isTest && reactRouter(), babel({ presets: [reactCompilerPreset()] })].filter(
-            Boolean,
-        ),
+        plugins: [
+            tailwindcss(),
+            react(),
+            VitePWA({
+                registerType: 'prompt',
+                injectRegister: false,
+
+                pwaAssets: {
+                    disabled: false,
+                    config: true,
+                },
+
+                manifest: {
+                    name: 'Utilities Webapp',
+                    short_name: 'Web Utilities',
+                    description:
+                        'A collection of web utilities built with React and Vite, including a password generator, calculators, and more.',
+                    start_url: '/',
+                    scope: '/',
+                    display: 'standalone',
+                    theme_color: 'oklch(0.6229 0.2012 35.9323)',
+                    background_color: 'oklch(0.9851 0 0)',
+                },
+
+                workbox: {
+                    globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+                    cleanupOutdatedCaches: true,
+                    clientsClaim: true,
+                },
+
+                devOptions: {
+                    enabled: false,
+                    navigateFallback: 'index.html',
+                    suppressWarnings: true,
+                    type: 'module',
+                },
+            }),
+            !isTest && reactRouter(),
+            babel({ presets: [reactCompilerPreset()] }),
+        ].filter(Boolean),
         build: {
             target: 'esnext',
             minify: 'oxc',
