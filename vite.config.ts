ac4ed45e70d1e4e3acb63cf677be3ebe10824281
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-06-21 13:29:41 -0300

    fix(vite): specify output directory for PWA build

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig(({ mode }) => {
         plugins: [
             tailwindcss(),
             VitePWA({
+                outDir: 'build/client',
                 registerType: 'prompt',
                 injectRegister: false,
 
