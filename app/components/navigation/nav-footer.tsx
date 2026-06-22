be439b65361a13b5df3249df5b328b374d8596b6
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-06-22 10:50:38 -0300

    feat(nav): add NavFooter component with sidebar menu and portfolio link

---
+++ app/components/navigation/nav-footer.tsx
@@ -0,0 +1,38 @@
+import { CodeXmlIcon } from 'lucide-react';
+import { Activity } from 'react';
+import { Link } from 'react-router';
+import { SidebarMenu, SidebarMenuItem, useSidebar } from '~/components/ui/sidebar';
+
+export function NavFooter() {
+    const { state, isMobile } = useSidebar();
+
+    return (
+        <SidebarMenu>
+            <SidebarMenuItem className="mx-auto grid">
+                <Activity mode={state === 'expanded' || isMobile ? 'visible' : 'hidden'}>
+                    <span aria-hidden className="text-center">
+                        &copy; {new Date().getFullYear()}
+                    </span>
+                </Activity>
+
+                <Link
+                    to="https://barataribeiro.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    tabIndex={isMobile && state === 'collapsed' ? -1 : 0}
+                    title="barata </> ribeiro"
+                    className="text-sm text-muted-foreground hover:text-foreground"
+                    aria-label="Visit my portfolio website"
+                >
+                    {state === 'collapsed' && !isMobile ? (
+                        <CodeXmlIcon aria-hidden />
+                    ) : (
+                        <span className="inline-flex items-center gap-x-1 font-medium">
+                            barata<span>&lt;/&gt;</span>ribeiro
+                        </span>
+                    )}
+                </Link>
+            </SidebarMenuItem>
+        </SidebarMenu>
+    );
+}
