51621b41df28bbee8214619e779fc15b2c366205
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-06-23 14:19:48 -0300

    feat(routes): add about route to application

--- app/routes.ts
+++ app/routes.ts
@@ -2,6 +2,7 @@ import { type RouteConfig, index, route } from '@react-router/dev/routes';
 
 export default [
     index('routes/home.tsx'),
+    route('/about', 'routes/about.tsx'),
     route('/theme', 'routes/theme.tsx'),
     route('/sidebar', 'routes/sidebar.tsx'),
 ] satisfies RouteConfig;
