ce4c5b96faad02a7b74b2cf47a03b2a0d22e9197
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-06-27 07:21:03 -0300

    feat(component): implement Base64 text decoding functionality

---
+++ app/components/pages/programming/base64-text-handlers/base64-text-decode.tsx
@@ -0,0 +1,107 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useState } from 'react';
+import { Controller, Resolver, useForm } from 'react-hook-form';
+import { z } from 'zod/v4';
+import ButtonClipboard from '~/components/button-clipboard';
+import { Button } from '~/components/ui/button';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '~/components/ui/card';
+import { Field, FieldError, FieldLabel } from '~/components/ui/field';
+import { Textarea } from '~/components/ui/textarea';
+
+const FormSchema = z.object({
+    base64: z.preprocess(
+        (val) => (typeof val === 'string' ? val.replace(/^data:(image\/[a-zA-Z]+);base64,/, '') : val),
+        z.base64('The provided value is not valid Base64.').min(1, 'Base64 string is required.'),
+    ),
+});
+
+type FormSchemaType = z.infer<typeof FormSchema>;
+
+export default function Base64TextDecode() {
+    const [result, setResult] = useState<string | null>(null);
+
+    const form = useForm<FormSchemaType>({
+        resolver: zodResolver(FormSchema) as Resolver<FormSchemaType>,
+        defaultValues: { base64: '' },
+    });
+
+    function onSubmit(data: FormSchemaType) {
+        const { base64 } = data;
+        setResult(atob(base64));
+    }
+
+    function resetForm() {
+        form.reset();
+        setResult(null);
+    }
+
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>Base64 Text Decode</CardTitle>
+                <CardDescription>Decode Base64 formatted text.</CardDescription>
+            </CardHeader>
+
+            <CardContent>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+                    <Controller
+                        control={form.control}
+                        name="base64"
+                        render={({ field, fieldState }) => (
+                            <Field data-invalid={fieldState.invalid}>
+                                <FieldLabel htmlFor="text-to-decode">Text</FieldLabel>
+                                <Textarea
+                                    id="text-to-decode"
+                                    className="max-h-32 resize-none font-mono"
+                                    placeholder="iVBORw0KGgoAAAANSUhEUgAA..."
+                                    aria-invalid={fieldState.invalid}
+                                    required
+                                    aria-required
+                                    {...field}
+                                />
+                                {fieldState.error && <FieldError errors={[fieldState.error]} />}
+                            </Field>
+                        )}
+                    />
+
+                    <div className="grid grid-rows-[auto_1fr] gap-2">
+                        <Button type="submit" variant="default">
+                            Decode
+                        </Button>
+
+                        <div className="grid gap-2 sm:grid-cols-2">
+                            <Button type="reset" variant="ghost" onClick={resetForm} aria-label="Reset Form">
+                                Reset
+                            </Button>
+
+                            <ButtonClipboard size="default" variant="outline" content={result} />
+                        </div>
+                    </div>
+                </form>
+            </CardContent>
+
+            <CardFooter className="w-full border-t pt-4">
+                {result ? (
+                    <div className="w-full space-y-1">
+                        <p className="font-semibold">Partial Result:</p>
+                        <pre className="rounded bg-muted p-2 font-mono text-sm break-all whitespace-pre-wrap">
+                            {result.substring(0, 250) + (result.length > 250 ? '...' : '')}
+                        </pre>
+                        <div className="flex flex-wrap items-center-safe justify-between gap-2">
+                            <p className="text-xs text-muted-foreground">
+                                Use the &ldquo;Copy&rdquo; button for complete result.
+                            </p>
+                            <p className="text-sm text-muted-foreground">
+                                Total Characters: <strong>{result.replaceAll(/\n/g, '').length}</strong>
+                            </p>
+                        </div>
+                    </div>
+                ) : (
+                    <p className="text-sm text-muted-foreground">
+                        Paste Base64 formatted text and click &ldquo;Decode&rdquo; to get the result.
+                    </p>
+                )}
+            </CardFooter>
+        </Card>
+    );
+}
