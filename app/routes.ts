7b3ac4d6379112f3b5aeb43a2cc0faf40e490613
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-06-27 18:56:24 -0300

    feat(routes): add route for Base64 to Image converter page

--- app/routes.ts
+++ app/routes.ts
@@ -15,6 +15,7 @@ export default [
     ...prefix('/programming', [
         index('routes/programming/index.tsx'),
         route('/base64-text-encode-decode', 'routes/programming/base64-text-encode-decode.tsx'),
+        route('/base64-to-image', 'routes/programming/base64-to-image.tsx'),
         route('/text-hashing', 'routes/programming/text-hashing.tsx'),
     ]),
     route('/theme', 'routes/theme.tsx'),
