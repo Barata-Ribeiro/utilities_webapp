5fae9a6a0d9c986bdc349cb5ed79452f0d3dfb69
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-07-01 17:13:12 -0300

    feat(dates): add DateTab component for date selection between Date Difference and Date Add/Subtract

---
+++ app/components/pages/calculators/date-tab.tsx
@@ -0,0 +1,29 @@
+import { CalendarRangeIcon, CalendarSyncIcon } from 'lucide-react';
+import DateAddSubtract from '~/components/pages/calculators/date/date-add-subtract';
+import { DateDifference } from '~/components/pages/calculators/date/date-difference';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '~/components/ui/tabs';
+
+export default function DateTab() {
+    return (
+        <Tabs defaultValue="date-difference" className="mx-auto w-full max-w-lg">
+            <TabsList className="flex h-auto! flex-wrap items-center justify-start gap-1">
+                <TabsTrigger value="date-difference">
+                    <CalendarRangeIcon aria-hidden size={16} />
+                    Date Difference
+                </TabsTrigger>
+                <TabsTrigger value="date-add-subtract">
+                    <CalendarSyncIcon aria-hidden size={16} />
+                    Date Add/Subtract
+                </TabsTrigger>
+            </TabsList>
+
+            <TabsContent value="date-difference">
+                <DateDifference />
+            </TabsContent>
+
+            <TabsContent value="date-add-subtract">
+                <DateAddSubtract />
+            </TabsContent>
+        </Tabs>
+    );
+}
