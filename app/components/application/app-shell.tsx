52f915d3d9f995af54b1b05fd60b206be6e08aed
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-08-03 06:23:49 -0300

    refactor(sidebar): enhance AppSidebar with mobile handling and location tracking

--- app/components/application/app-shell.tsx
+++ app/components/application/app-shell.tsx
@@ -23,7 +23,7 @@ export default function AppShell({ children, theme, sidebarOpen }: Readonly<Prop
                     <AppSidebar />
 
                     <SidebarInset>
-                        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+                        <header className="flex h-16 shrink-0 items-center gap-2 shadow transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
                             <div className="flex items-center gap-2 px-4">
                                 <SidebarTrigger className="-ml-1" />
                                 <Separator orientation="vertical" className="mr-2" />
@@ -31,7 +31,7 @@ export default function AppShell({ children, theme, sidebarOpen }: Readonly<Prop
                                 <Breadcrumbs />
                             </div>
                         </header>
-                        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+                        <div className="mt-4 flex flex-1 flex-col gap-4 p-4 pt-0">
                             <main className="relative flex min-h-0 flex-1 flex-col gap-4 md:gap-6">{children}</main>
                         </div>
                     </SidebarInset>
--- app/components/application/app-sidebar.tsx
+++ app/components/application/app-sidebar.tsx
@@ -1,10 +1,29 @@
-import { type ComponentProps } from 'react';
+import { useEffect, useEffectEvent, type ComponentProps } from 'react';
+import { useLocation } from 'react-router';
 import { Logo } from '~/components/navigation/logo';
 import { NavFooter } from '~/components/navigation/nav-footer';
 import { NavMain } from '~/components/navigation/nav-main';
-import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarRail } from '~/components/ui/sidebar';
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarHeader,
+    SidebarRail,
+    useSidebar,
+} from '~/components/ui/sidebar';
 
 export function AppSidebar({ ...props }: ComponentProps<typeof Sidebar>) {
+    const { isMobile, setOpenMobile } = useSidebar();
+    const { pathname } = useLocation();
+
+    const onPathnameChange = useEffectEvent(() => {
+        if (isMobile) {
+            setOpenMobile(false);
+        }
+    });
+
+    useEffect(() => onPathnameChange(), [pathname]);
+
     return (
         <Sidebar collapsible="icon" variant="sidebar" {...props}>
             <SidebarHeader>
