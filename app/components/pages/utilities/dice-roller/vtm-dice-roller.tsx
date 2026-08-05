a1543309ef4984a925c00b290ca7fcace5d16b68
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-08-05 10:16:07 -0300

    feat(dice-roller): enhance VtmDiceRoller with rollDice function and UI updates

--- app/components/pages/utilities/dice-roller/vtm-dice-roller.tsx
+++ app/components/pages/utilities/dice-roller/vtm-dice-roller.tsx
@@ -1,71 +1,140 @@
 import { zodResolver } from '@hookform/resolvers/zod';
-import { type BaseSyntheticEvent, useCallback } from 'react';
+import { InfoIcon } from 'lucide-react';
+import { type BaseSyntheticEvent, useCallback, useState, useTransition } from 'react';
 import { Controller, type Resolver, useForm } from 'react-hook-form';
 import { z } from 'zod/v4';
+import { Alert, AlertDescription, AlertTitle } from '~/components/ui/alert';
 import { Button } from '~/components/ui/button';
 import { ButtonGroup } from '~/components/ui/button-group';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '~/components/ui/card';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '~/components/ui/card';
 import { Field, FieldError, FieldGroup, FieldLabel } from '~/components/ui/field';
 import { Input } from '~/components/ui/input';
 import useIsMobile from '~/hooks/use-mobile';
+import { rollDie } from '~/lib/dice-roller.utils';
+
+type DiceRollData = {
+    regularDiceRoll: number[];
+    hungerDiceRoll: number[];
+};
 
 const VtmDiceRollerSchema = z.object({
-    regularDice: z.coerce.number().min(1).max(20).default(1),
-    hungerDice: z.coerce.number().min(0).max(20).default(0),
-    difficulty: z.coerce.number().min(2).max(10).default(6).optional(),
+    regularDice: z.coerce.number().min(1).max(10).default(1),
+    hungerDice: z.coerce.number().min(0).max(5).default(0),
+    difficulty: z.coerce.number().min(2).max(10).default(4).optional(),
 });
 
 type VtmDiceRollerSchemaType = z.infer<typeof VtmDiceRollerSchema>;
 
 export default function VtmDiceRoller() {
+    const [currentRoll, setCurrentRoll] = useState<DiceRollData>({ regularDiceRoll: [], hungerDiceRoll: [] });
+    const [isPending, startTransition] = useTransition();
     const { isMobile } = useIsMobile();
 
     const form = useForm<VtmDiceRollerSchemaType>({
         resolver: zodResolver(VtmDiceRollerSchema) as Resolver<VtmDiceRollerSchemaType>,
-        defaultValues: { regularDice: 1, hungerDice: 0, difficulty: 6 },
+        defaultValues: { regularDice: 1, hungerDice: 0, difficulty: 4 },
     });
 
-    const onSubmitFn = useCallback(async (data: VtmDiceRollerSchemaType, event?: BaseSyntheticEvent) => {
-        const { regularDice, hungerDice, difficulty } = data;
-        const buttonClicked = (event?.nativeEvent as SubmitEvent | undefined)?.submitter?.id;
-
-        if (buttonClicked === 'roll-dice') {
-            console.log('Rolling dice with the following parameters:');
-            console.log(`Total Dice: ${regularDice}`);
-            console.log(`Hunger Dice: ${hungerDice}`);
-            console.log(`Difficulty: ${difficulty}`);
-        } else if (buttonClicked === 'rouse-check') {
-            console.log('Performing a rouse check with the following parameters:');
-            console.log(`Total Dice: ${regularDice}`);
-            console.log(`Hunger Dice: ${hungerDice}`);
+    function rollDice(regularDice: number, hungerDice: number, difficulty: number | undefined) {
+        if (hungerDice > regularDice) {
+            alert('Hunger dice cannot exceed total dice.');
+            return;
         }
-    }, []);
+
+        if (difficulty && (difficulty < 2 || difficulty > 10)) {
+            alert('Difficulty must be between 2 and 10.');
+            return;
+        }
+
+        if (regularDice < 1 || regularDice > 10) {
+            alert('Total dice must be between 1 and 10.');
+            return;
+        }
+
+        if (hungerDice < 0 || hungerDice > 5) {
+            alert('Hunger dice must be between 0 and 5.');
+            return;
+        }
+
+        setCurrentRoll({ regularDiceRoll: [], hungerDiceRoll: [] });
+
+        for (let i = 0; i < regularDice; i++) {
+            const roll = rollDie(10);
+            setCurrentRoll((prev) => ({ ...prev, regularDiceRoll: [...prev.regularDiceRoll, roll] }));
+        }
+
+        for (let i = 0; i < hungerDice; i++) {
+            const roll = rollDie(10);
+            setCurrentRoll((prev) => ({ ...prev, hungerDiceRoll: [...prev.hungerDiceRoll, roll] }));
+        }
+    }
+
+    const onSubmitFn = useCallback(
+        (data: VtmDiceRollerSchemaType, event?: BaseSyntheticEvent) => {
+            startTransition(async () => {
+                const { regularDice, hungerDice, difficulty } = data;
+                const buttonClicked = (event?.nativeEvent as SubmitEvent | undefined)?.submitter?.id;
+
+                switch (buttonClicked) {
+                    case 'roll-dice':
+                        rollDice(regularDice, hungerDice, difficulty);
+                        break;
+                    case 'rouse-check':
+                        console.log(
+                            `Performing rouse check with ${regularDice} regular dice and ${hungerDice} hunger dice.`,
+                        );
+                        break;
+                    default:
+                        throw new Error('Something went wrong. Please try again.');
+                }
+            });
+        },
+        [startTransition],
+    );
 
     function reset() {
         form.reset();
+        setCurrentRoll({ regularDiceRoll: [], hungerDiceRoll: [] });
     }
 
     return (
         <Card>
             <CardHeader className="border-b">
                 <CardTitle>Vampire: The Masquerade (5th edition) Dice Roller</CardTitle>
-                <CardDescription>Roll normal and hunger dice for Vampire: The Masquerade.</CardDescription>
+                <CardDescription>
+                    Roll regular and hunger dice for Vampire: The Masquerade (5th edition) using this simple dice
+                    roller.
+                </CardDescription>
             </CardHeader>
 
-            <CardContent>
-                <form onSubmit={form.handleSubmit(onSubmitFn)} className="flex flex-col gap-4">
+            <CardContent className="space-y-4">
+                <Alert className="bg-muted/50">
+                    <InfoIcon />
+                    <AlertTitle>Important!</AlertTitle>
+                    <AlertDescription>
+                        Regular dice are summed with hunger dice to determine the total dice rolled. Hunger dice are a
+                        special type of die that can result in critical successes or failures, so read the rules
+                        carefully!
+                    </AlertDescription>
+                </Alert>
+
+                <form
+                    onSubmit={form.handleSubmit(onSubmitFn)}
+                    className="flex flex-col gap-4 inert:grayscale-100"
+                    inert={isPending ? true : undefined}
+                >
                     <FieldGroup className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
                         <Controller
                             control={form.control}
                             name="regularDice"
                             render={({ field, fieldState }) => (
                                 <Field data-invalid={fieldState.invalid} className="relative">
-                                    <FieldLabel htmlFor={field.name}>Total Dice</FieldLabel>
+                                    <FieldLabel htmlFor={field.name}>Regular Dice</FieldLabel>
                                     <Input
                                         type="number"
                                         id={field.name}
                                         min={1}
-                                        max={20}
+                                        max={10}
                                         step={1}
                                         inputMode="decimal"
                                         placeholder="e.g. 5"
@@ -92,7 +161,7 @@ export default function VtmDiceRoller() {
                                         type="number"
                                         id={field.name}
                                         min={0}
-                                        max={20}
+                                        max={5}
                                         step={1}
                                         inputMode="decimal"
                                         placeholder="e.g. 3"
@@ -151,6 +220,25 @@ export default function VtmDiceRoller() {
                     </ButtonGroup>
                 </form>
             </CardContent>
+            <CardFooter className="border-t">
+                {currentRoll.regularDiceRoll.length > 0 || currentRoll.hungerDiceRoll.length > 0 ? (
+                    <div className="flex flex-col gap-2">
+                        <p className="text-sm font-medium">Current Roll:</p>
+                        <div className="flex flex-wrap gap-2">
+                            {currentRoll.regularDiceRoll.map((roll, index) => (
+                                <span key={`regular-dice-${index}-${roll}`}>{roll}</span>
+                            ))}
+                        </div>
+                        {currentRoll.hungerDiceRoll.length > 0 && (
+                            <div className="flex flex-wrap gap-2">
+                                {currentRoll.hungerDiceRoll.map((roll, index) => (
+                                    <span key={`hunger-dice-${index}-${roll}`}>{roll}</span>
+                                ))}
+                            </div>
+                        )}
+                    </div>
+                ) : null}
+            </CardFooter>
         </Card>
     );
 }
