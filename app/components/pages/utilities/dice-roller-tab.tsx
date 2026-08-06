45576ea511fc02fcd6dfc8b7ea34f0e3b4716234
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-08-06 13:32:37 -0300

    chore(rename): rename AnkhIcon component and move to proper folder

--- app/components/pages/utilities/dice-roller-tab.tsx
+++ app/components/pages/utilities/dice-roller-tab.tsx
@@ -1,22 +1,24 @@
 import { DicesIcon } from 'lucide-react';
-import AnkhIcon from '~/components/AnkhIcon';
+import AnkhIcon from '~/components/icons/ankh.icon';
+import GenericDiceRoller from '~/components/pages/utilities/dice-roller/generic-dice-roller';
 import VtmDiceRoller from '~/components/pages/utilities/dice-roller/vtm-dice-roller';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '~/components/ui/tabs';
 
 export default function DiceRollerTab() {
-    // TODO: Change defaultValue to 'regular' once implemented
     return (
-        <Tabs defaultValue="vtm" className="mx-auto w-full max-w-lg">
+        <Tabs defaultValue="generic" className="mx-auto w-full max-w-lg">
             <TabsList className="flex h-auto! flex-wrap items-center justify-start gap-1">
-                <TabsTrigger value="regular">
-                    <DicesIcon aria-hidden size={16} /> Regular Dice
+                <TabsTrigger value="generic">
+                    <DicesIcon aria-hidden size={16} /> Dice Roller
                 </TabsTrigger>
                 <TabsTrigger value="vtm">
                     <AnkhIcon aria-hidden className="h-4" /> Vampire: The Masquerade
                 </TabsTrigger>
             </TabsList>
 
-            <TabsContent value="regular">{/* Regular Dice content goes here */}</TabsContent>
+            <TabsContent value="generic">
+                <GenericDiceRoller />
+            </TabsContent>
             <TabsContent value="vtm">
                 <VtmDiceRoller />
             </TabsContent>
